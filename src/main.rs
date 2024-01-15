00d977c47484ce3b51b925d88b3cfee957dbad8f
POP303U <nicoanton.gering@gmail.com>
2024-01-16 00:18:54 +0100

    Feat: Make it lobotomy

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
 use bevy::prelude::*;
+use bevy::window::PresentMode;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
+pub const CLEAR: Color = Color::rgba(0.1, 0.1, 0.1, 0.1);
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "I am a window!".into(),
+                resolution: (500., 300.).into(),
+                present_mode: PresentMode::AutoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(CLEAR))
+        .add_systems(Startup, spawn_cam)
+        .add_systems(Startup, spawn_sprite)
+        .add_systems(Startup, spawn_audio)
         .run();
 }
 
-fn hello_world() {
-    println!("Hello World!");
+fn spawn_cam(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Tom".to_string())));
-    commands.spawn((Person, Name("Tim".to_string())));
-    commands.spawn((Person, Name("Tum".to_string())));
+fn spawn_sprite(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("lobotomy.png"),
+        ..default()
+    });
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}!", name.0);
-    }
+fn spawn_audio(asset_server: Res<AssetServer>, mut commands: Commands) {
+    commands.spawn(AudioBundle {
+        source: asset_server.load("lobotomy-dash.ogg"),
+        ..default()
+    });
 }
